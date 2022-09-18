1503decac0acc5a9f12a1e14c6a4363dcbd90c9b
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-09-18 17:24:47 +0300

    Launches a node from a State Sync snapshot

--- State_Sync/state_sync.sh
+++ State_Sync/state_sync.sh
@@ -38,7 +38,7 @@ while test $# -gt 0; do
 		echo -e "${C_LGn}Useful URLs${RES}:"
 		echo -e "https://github.com/SecorD0/Cosmos/blob/main/State_Sync/state_sync.sh — script URL"
 		echo -e "         (you can add a new or delete irrelevant snapshot node via PR)"
-		echo -e "https://t.me/letskynode — node Community"
+		echo -e "https://t.me/OnePackage — noderun and tech community"
 		echo
 		return 0 2>/dev/null; exit 0
 		;;
